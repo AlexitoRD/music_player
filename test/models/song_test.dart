628ad61654d4de4dcee2ad43e2dc658dfaca2dcf
ardun <a.r.dunbar91@gmail.com>
2022-11-03 22:29:57 -0300

    Song toMap test added.

--- test/models/song_test.dart
+++ test/models/song_test.dart
@@ -3,6 +3,8 @@
  * Date: 2022-10-29, 1:16 a.m.
  */
 
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:music_player/models/song.dart';
 
@@ -10,5 +12,26 @@ void main() {
   test('Doing a unit test', () async {
     final song = Song(filePath: 'to_file', artist: 'Phil Collins');
     expect(song.artist, 'Phil Collins');
+  });
+
+  test('Test can create a map from a song', () async {
+    final song = Song(
+        filePath: 'file_path',
+        id: 1,
+        title: 'title',
+        artist: 'artist',
+        album: 'album',
+        year: 1991,
+        albumArt: Uint8List(3));
+    final result = song.toMap();
+    expect(result, {
+      'id': 1,
+      'title': 'title',
+      'artist': 'artist',
+      'album': 'album',
+      'year': 1991,
+      'filePath': 'file_path',
+      'albumArt': [0, 0, 0]
+    });
   });
 }
